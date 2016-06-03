3a881bce8993162b9283e4ff94ab9146267c1008
tatsy <tatsy.mail@gmail.com>
2016-06-03 16:59:34 +0900

    Revise code to remove deprecated functions of OpenMesh.

--- sources/denoising.cpp
+++ sources/denoising.cpp
@@ -12,185 +12,174 @@ typedef Eigen::SparseMatrix<double> SpMat;
 typedef Eigen::Triplet<double> Triplet;
 
 bool operator<(const Vector3d& v1, const Vector3d& v2) {
-	if(v1[0] != v2[0]) return v1[0] < v2[0];
-	if(v1[1] != v2[1]) return v1[1] < v2[1];
-	return v1[2] < v1[2];
+    if(v1[0] != v2[0]) return v1[0] < v2[0];
+    if(v1[1] != v2[1]) return v1[1] < v2[1];
+    return v1[2] < v1[2];
 }
 
 bool operator>(const Vector3d& v1, const Vector3d& v2) {
-	if(v1[0] != v2[0]) return v1[0] > v2[0];
-	if(v1[1] != v2[1]) return v1[1] > v2[1];
-	return v1[2] > v1[2];
+    if(v1[0] != v2[0]) return v1[0] > v2[0];
+    if(v1[1] != v2[1]) return v1[1] > v2[1];
+    return v1[2] > v1[2];
 }
 
 double genrand_gauss(double mu, double sigma) {
-	double z = sqrt(-2.0 * log(genrand_real2())) * sin(2.0 * M_PI * genrand_real2());
-	return mu + sigma * z;
+    double z = sqrt(-2.0 * log(genrand_real2())) * sin(2.0 * M_PI * genrand_real2());
+    return mu + sigma * z;
 }
 
 void addNoise(Mesh& mesh) {
-	init_genrand((unsigned long)time(NULL));
-	OpenMesh::VPropHandleT<Mesh::Point> vprop;
-	mesh.add_property(vprop);
+    init_genrand((unsigned long)time(NULL));
+    OpenMesh::VPropHandleT<Mesh::Point> vprop;
+    mesh.add_property(vprop);
 
-	Mesh::VertexIter v_it;
-	for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
-		double vx = genrand_gauss(0.0, 0.001);
-		double vy = genrand_gauss(0.0, 0.001);
-		double vz = genrand_gauss(0.0, 0.001);
-		mesh.property(vprop, v_it) = mesh.point(v_it) + Mesh::Point(vx, vy, vz);
-		mesh.set_point(v_it, mesh.property(vprop, v_it));
-	}
-	mesh.remove_property(vprop);
+    Mesh::VertexIter v_it;
+    for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
+        double vx = genrand_gauss(0.0, 0.001);
+        double vy = genrand_gauss(0.0, 0.001);
+        double vz = genrand_gauss(0.0, 0.001);
+        mesh.property(vprop, *v_it) = mesh.point(*v_it) + Mesh::Point(vx, vy, vz);
+        mesh.set_point(*v_it, mesh.property(vprop, *v_it));
+    }
+    mesh.remove_property(vprop);
 }
 
 void denoise(Mesh& mesh) {
-	// まず係数行列 R, D を用意する
-	Mesh::EdgeIter e_it;
-	Mesh::FaceVertexIter fv_it;
+    // Prepare coefficient matrices R and D.
+    Mesh::EdgeIter e_it;
+    Mesh::FaceVertexIter fv_it;
 
-	int nVert = mesh.n_vertices();
-	int nEdge = mesh.n_edges();
+    const int nVert = static_cast<int>(mesh.n_vertices());
+    const int nEdge = static_cast<int>(mesh.n_edges());
 
-	vector<Triplet> tripR;
-	vector<Triplet> tripD;
+    vector<Triplet> tripR;
+    vector<Triplet> tripD;
 
-	int ie = 0;
-	for(e_it = mesh.edges_begin(); e_it != mesh.edges_end(); ++e_it) {
-		Mesh::HalfedgeHandle heh = mesh.halfedge_handle(e_it.handle(), 0);
+    int ie = 0;
+    for(e_it = mesh.edges_begin(); e_it != mesh.edges_end(); ++e_it) {
+        Mesh::HalfedgeHandle heh = mesh.halfedge_handle(*e_it, 0);
 
-		// 順番が変だけどこれであっている
-		Mesh::VertexHandle vh1 = mesh.to_vertex_handle(heh);
-		Mesh::VertexHandle vh3 = mesh.from_vertex_handle(heh);
-		Mesh::VertexHandle vh2 = mesh.opposite_vh(heh);
-		Mesh::VertexHandle vh4 = mesh.opposite_he_opposite_vh(heh);
+        Mesh::VertexHandle vh1 = mesh.to_vertex_handle(heh);
+        Mesh::VertexHandle vh3 = mesh.from_vertex_handle(heh);
+        Mesh::VertexHandle vh2 = mesh.opposite_vh(heh);
+        Mesh::VertexHandle vh4 = mesh.opposite_he_opposite_vh(heh);
 
-		int i1 = vh1.idx();
-		int i2 = vh2.idx();
-		int i3 = vh3.idx();
-		int i4 = vh4.idx();
-		if(i1 == -1 || i2 == -1 || i3 == -1 || i4 == -1) {
-			continue;
-		}
+        int i1 = vh1.idx();
+        int i2 = vh2.idx();
+        int i3 = vh3.idx();
+        int i4 = vh4.idx();
+        if(i1 == -1 || i2 == -1 || i3 == -1 || i4 == -1) {
+            continue;
+        }
 
-		Mesh::Point p1 = mesh.point(vh1);
-		Mesh::Point p2 = mesh.point(vh2);
-		Mesh::Point p3 = mesh.point(vh3);
-		Mesh::Point p4 = mesh.point(vh4);
+        OpenMesh::Vec3f p1 = mesh.point(vh1);
+        OpenMesh::Vec3f p2 = mesh.point(vh2);
+        OpenMesh::Vec3f p3 = mesh.point(vh3);
+        OpenMesh::Vec3f p4 = mesh.point(vh4);
 
-		double S123 = 0.5 * OpenMesh::cross(p2 - p1, p2 - p3).norm();
-		double S134 = 0.5 * OpenMesh::cross(p4 - p1, p4 - p3).norm();
-		double l13  = (p1 - p3).norm();
+        double S123 = 0.5 * OpenMesh::cross(p2 - p1, p2 - p3).norm();
+        double S134 = 0.5 * OpenMesh::cross(p4 - p1, p4 - p3).norm();
+        double l13  = (p1 - p3).norm();
 
-		double coef1 = (S123 * OpenMesh::dot(p4 - p3, p3 - p1) + S134 * OpenMesh::dot(p1 - p3, p3 - p2)) / (l13 * l13 * (S123 + S134));
-		double coef2 = S134 / (S123 + S134);
-		double coef3 = (S123 * OpenMesh::dot(p3 - p1, p1 - p4) + S134 * OpenMesh::dot(p2 - p1, p1 - p3)) / (l13 * l13 * (S123 + S134));
-		double coef4 = S123 / (S123 + S134);
+        double coef1 = (S123 * OpenMesh::dot(p4 - p3, p3 - p1) + S134 * OpenMesh::dot(p1 - p3, p3 - p2)) / (l13 * l13 * (S123 + S134));
+        double coef2 = S134 / (S123 + S134);
+        double coef3 = (S123 * OpenMesh::dot(p3 - p1, p1 - p4) + S134 * OpenMesh::dot(p2 - p1, p1 - p3)) / (l13 * l13 * (S123 + S134));
+        double coef4 = S123 / (S123 + S134);
 
-		tripR.push_back(Triplet(ie, i1, coef1));
-		tripR.push_back(Triplet(ie, i2, coef2));
-		tripR.push_back(Triplet(ie, i3, coef3));
-		tripR.push_back(Triplet(ie, i4, coef4));
+        tripR.push_back(Triplet(ie, i1, coef1));
+        tripR.push_back(Triplet(ie, i2, coef2));
+        tripR.push_back(Triplet(ie, i3, coef3));
+        tripR.push_back(Triplet(ie, i4, coef4));
 
-		tripD.push_back(Triplet(ie, i1, 1.0));
-		tripD.push_back(Triplet(ie, i2, -1.0));
-		tripD.push_back(Triplet(ie, i3, 1.0));
-		tripD.push_back(Triplet(ie, i4, -1.0));
-		ie++;
-	}
+        tripD.push_back(Triplet(ie, i1, 1.0));
+        tripD.push_back(Triplet(ie, i2, -1.0));
+        tripD.push_back(Triplet(ie, i3, 1.0));
+        tripD.push_back(Triplet(ie, i4, -1.0));
+        ie++;
+    }
 
-	SpMat R(nEdge, nVert);
-	SpMat D(nEdge, nVert);
-	R.setFromTriplets(tripR.begin(), tripR.end());
-	D.setFromTriplets(tripD.begin(), tripD.end());
+    SpMat R(nEdge, nVert);
+    SpMat D(nEdge, nVert);
+    R.setFromTriplets(tripR.begin(), tripR.end());
+    D.setFromTriplets(tripD.begin(), tripD.end());
 
-	// 点の座標をベクトルに入れる
-	Matrix pInit(nVert, 3);
-	Matrix pVec(nVert, 3);
-	Mesh::VertexIter v_it;
-	int ip = 0;
-	for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
-		Mesh::Point p = mesh.point(v_it.handle());
-		pInit(ip, 0) = p[0];
-		pInit(ip, 1) = p[1];
-		pInit(ip, 2) = p[2];
+    // Store vertex positions in the Eigen::Vector
+    Matrix pInit(nVert, 3);
+    Matrix pVec(nVert, 3);
+    Mesh::VertexIter v_it;
+    int ip = 0;
+    for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
+        Mesh::Point p = mesh.point(*v_it);
+        pInit(ip, 0) = p[0];
+        pInit(ip, 1) = p[1];
+        pInit(ip, 2) = p[2];
 
-		pVec(ip, 0) = p[0];
-		pVec(ip, 1) = p[1];
-		pVec(ip, 2) = p[2];
-		ip++;
-	}
+        pVec(ip, 0) = p[0];
+        pVec(ip, 1) = p[1];
+        pVec(ip, 2) = p[2];
+        ip++;
+    }
 
-	// 疎な単位行列の用意
-	vector<Triplet> tripI(nVert);
-	for(int i=0; i<nVert; i++) {
-		tripI[i] = Triplet(i, i, 1.0);
-	}
-	SpMat I(nVert, nVert);
-	I.setFromTriplets(tripI.begin(), tripI.end());
+    // Prepare sparse matrix.
+    vector<Triplet> tripI(nVert);
+    for(int i=0; i<nVert; i++) {
+        tripI[i] = Triplet(i, i, 1.0);
+    }
+    SpMat I(nVert, nVert);
+    I.setFromTriplets(tripI.begin(), tripI.end());
 
-	// 最適化問題を解く
-	double lambda = 1.0e-2;
-	double alpha  = 0.1;
-	double beta   = 1.0e-3;
-	double bmax   = 1.0;
-	double mu     = 2.0;
+    // Solve linear problem.
+    double lambda = 1.0e-2;
+    double alpha  = 0.1;
+    double beta   = 1.0e-3;
+    double bmax   = 1.0;
+    double mu     = 2.0;
 
-	// Matrix pVec = pInit;
-	Matrix dlt(nEdge, 3);
-	while(beta < bmax) {
-		// solve sub-problem for delta (shrinkage operator)
-		Matrix y = D * pVec;
-		for(int i=0; i<nEdge; i++) {
-			dlt(i, 0) = 0.0;
-			for(int d=0; d<3; d++) {
-				dlt(i, 0) += y(i, d) * y(i, d);
-			}
-			
-			if(dlt(i, 0) < lambda / beta) {
-				dlt(i, 0) = 0.0;
-			}
+    // Matrix pVec = pInit;
+    Matrix dlt(nEdge, 3);
+    while(beta < bmax) {
+        // solve sub-problem for delta (shrinkage operator)
+        Matrix y = D * pVec;
+        for(int i=0; i<nEdge; i++) {
+            dlt(i, 0) = 0.0;
+            for(int d=0; d<3; d++) {
+                dlt(i, 0) += y(i, d) * y(i, d);
+            }
+            
+            if(dlt(i, 0) < lambda / beta) {
+                dlt(i, 0) = 0.0;
+            }
 
-			dlt(i, 1) = dlt(i, 0);
-			dlt(i, 2) = dlt(i, 0);
-		}
+            dlt(i, 1) = dlt(i, 0);
+            dlt(i, 2) = dlt(i, 0);
+        }
 
-		// solve sub-problem for points (linear system)
-		SpMat  A = I + alpha * R.transpose() * R + beta * D.transpose() * D;
-		Matrix b = pInit + beta * D.transpose() * dlt;
+        // solve sub-problem for points (linear system)
+        SpMat  A = I + alpha * R.transpose() * R + beta * D.transpose() * D;
+        Matrix b = pInit + beta * D.transpose() * dlt;
 
-		Eigen::ConjugateGradient<SpMat> solver;
-		solver.compute(A);
-		pVec = solver.solve(b);
-		if(solver.info() != Eigen::Success) {
-			printf("Solve linear system failed.\n");
-		}
+        Eigen::ConjugateGradient<SpMat> solver;
+        solver.compute(A);
+        pVec = solver.solve(b);
+        if(solver.info() != Eigen::Success) {
+            printf("Solve linear system failed.\n");
+        }
+        
+        // update parameters
+        printf(".");
+        beta  *= mu;
+        alpha *= 0.5;
+    }
+    printf("\n");
 
-		// compute difference
-		//double diff = 0.0;
-		//for(int i=0; i<nVert; i++) {
-		//	for(int d=0; d<3; d++) {
-		//		double del = pInit(i, d) - pVec(i, d);
-		//		diff += del * del;
-		//	}
-		//}
-		//printf("%f\n", diff);
-		
-		// update parameters
-		printf(".");
-		beta  *= mu;
-		alpha *= 0.5;
-	}
-	printf("\n");
-
-	// 点の情報を更新
-	OpenMesh::VPropHandleT<Mesh::Point> vprop;
-	mesh.add_property(vprop);
-	ip = 0;
-	for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
-		mesh.property(vprop, v_it) = Mesh::Point(pVec(ip, 0), pVec(ip, 1), pVec(ip, 2));
-		mesh.set_point(v_it, mesh.property(vprop, v_it));
-		ip++;
-	}
-	mesh.remove_property(vprop);
+    // Update vertex information_OBJMESH_H_
+    OpenMesh::VPropHandleT<Mesh::Point> vprop;
+    mesh.add_property(vprop);
+    ip = 0;
+    for(v_it = mesh.vertices_begin(); v_it != mesh.vertices_end(); ++v_it) {
+        mesh.property(vprop, *v_it) = Mesh::Point(pVec(ip, 0), pVec(ip, 1), pVec(ip, 2));
+        mesh.set_point(*v_it, mesh.property(vprop, *v_it));
+        ip++;
+    }
+    mesh.remove_property(vprop);
 }
--- sources/main.cpp
+++ sources/main.cpp
@@ -19,170 +19,170 @@ const int    WIN_WIDTH  = 700;
 const int    WIN_HEIGHT = 700;
 
 void description() {
-	cout << "*** Mesh Denoising via L0 Minimization ***" << endl;
-	cout << "[Keyboard]" << endl;
-	cout << "  d: denoising via L0 minimization" << endl;
-	cout << "  n: add noise to the mesh" << endl;
-	cout << "  s: save current image" << endl;
-	cout << endl;
+    cout << "*** Mesh Denoising via L0 Minimization ***" << endl;
+    cout << "[Keyboard]" << endl;
+    cout << "  d: denoising via L0 minimization" << endl;
+    cout << "  n: add noise to the mesh" << endl;
+    cout << "  s: save current image" << endl;
+    cout << endl;
 }
 
 void loadMesh(const std::string& filename) {
-	if(!OpenMesh::IO::read_mesh(mesh, filename)) {
-		cerr << "Failed to load file \"" << filename << "\"" << endl;
-	}
+    if(!OpenMesh::IO::read_mesh(mesh, filename)) {
+        cerr << "Failed to load file \"" << filename << "\"" << endl;
+    }
 }
 
 void renderMesh() {
-	Mesh::FaceIter f_it;
-	Mesh::FaceVertexIter fv_it;
+    Mesh::FaceIter f_it;
+    Mesh::FaceVertexIter fv_it;
 
-	glBegin(GL_TRIANGLES);
-	for(f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it) {
-		fv_it = mesh.fv_iter(f_it.handle());
+    glBegin(GL_TRIANGLES);
+    for(f_it = mesh.faces_begin(); f_it != mesh.faces_end(); ++f_it) {
+        fv_it = mesh.fv_iter(*f_it);
 
-		Mesh::Point normal = mesh.calc_face_normal(f_it.handle());
-		glNormal3d(normal[0], normal[1], normal[2]);
-		for(; fv_it; ++fv_it) {
-			Mesh::Point pt = mesh.point(fv_it.handle());
-			glVertex3d(pt[0], pt[1], pt[2]);
-		}
-	}
-	glEnd();
+        Mesh::Point normal = mesh.calc_face_normal(*f_it);
+        glNormal3d(normal[0], normal[1], normal[2]);
+        for(; fv_it.is_valid(); ++fv_it) {
+            Mesh::Point pt = mesh.point(*fv_it);
+            glVertex3d(pt[0], pt[1], pt[2]);
+        }
+    }
+    glEnd();
 }
 
 void glSetMaterial() {
-	static float ambient[]  = { 0.24725, 0.1995, 0.0745, 1.0 };
-	static float diffuse[]  = { 0.75164, 0.60648 , 0.22648, 1.0 };
-	static float specular[] = { 0.628281, 0.555802,0.366065,1.0 };
-	static float shineness  =  51.2;
+    static float ambient[]  = { 0.2472f, 0.1995f, 0.0745f, 1.0f };
+    static float diffuse[]  = { 0.7516f, 0.6064f, 0.2264f, 1.0f };
+    static float specular[] = { 0.6282f, 0.5558f, 0.3660f, 1.0f };
+    static float shineness  =  51.2f;
 
-	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
-	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
-	glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shineness);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambient);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffuse);
+    glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specular);
+    glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shineness);
 }
 
 void saveImage() {
-	unsigned char* pixel_data = new unsigned char[WIN_WIDTH * WIN_HEIGHT * 3];
-	glReadBuffer(GL_FRONT);
-	glReadPixels(0, 0, WIN_WIDTH, WIN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (void*)pixel_data);
+    unsigned char* pixel_data = new unsigned char[WIN_WIDTH * WIN_HEIGHT * 3];
+    glReadBuffer(GL_FRONT);
+    glReadPixels(0, 0, WIN_WIDTH, WIN_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, (void*)pixel_data);
 
-	cv::Mat img(WIN_HEIGHT, WIN_WIDTH, CV_8UC3);
-	for(int y=0; y<WIN_HEIGHT; y++) {
-		for(int x=0; x<WIN_WIDTH; x++) {
-			for(int d=0; d<3; d++) {
-				img.data[(y*WIN_WIDTH+x)*3+d] = pixel_data[((WIN_HEIGHT-y-1)*WIN_WIDTH+x)*3+(2-d)];
-			}
-		}
-	}
+    cv::Mat img(WIN_HEIGHT, WIN_WIDTH, CV_8UC3);
+    for(int y=0; y<WIN_HEIGHT; y++) {
+        for(int x=0; x<WIN_WIDTH; x++) {
+            for(int d=0; d<3; d++) {
+                img.data[(y*WIN_WIDTH+x)*3+d] = pixel_data[((WIN_HEIGHT-y-1)*WIN_WIDTH+x)*3+(2-d)];
+            }
+        }
+    }
 
-	string outfile;
-	cout << "save as: ";
-	cin >> outfile;
+    string outfile;
+    cout << "save as: ";
+    cin >> outfile;
 
-	cv::imwrite(outfile, img);
-	cout << "Successfully saved to \"" << outfile << "\"" << endl;
-	delete [] pixel_data;
+    cv::imwrite(outfile, img);
+    cout << "Successfully saved to \"" << outfile << "\"" << endl;
+    delete [] pixel_data;
 }
 
 void keyboard(unsigned char key, int x, int y) {
-	switch(key) {
-	case 'd':
-		denoise(mesh);
-		break;
+    switch(key) {
+    case 'd':
+        denoise(mesh);
+        break;
 
-	case 'n':
-		addNoise(mesh);
-		break;
+    case 'n':
+        addNoise(mesh);
+        break;
 
-	case 's':
-		saveImage();
-		break;
+    case 's':
+        saveImage();
+        break;
 
-	case 0x1b:
-		exit(0);
-		break;
-	}
-	glutPostRedisplay();
+    case 0x1b:
+        exit(0);
+        break;
+    }
+    glutPostRedisplay();
 }
 
 void mouse(int button, int state, int x, int y) {
-	if(button == GLUT_LEFT_BUTTON) {
-		if(state == GLUT_DOWN) {
-			isPress = true;
-			prevX = x;
-			prevY = y;
-		}
+    if(button == GLUT_LEFT_BUTTON) {
+        if(state == GLUT_DOWN) {
+            isPress = true;
+            prevX = x;
+            prevY = y;
+        }
 
-		if(state == GLUT_UP) {
-			isPress = false;
-		}
-	}
+        if(state == GLUT_UP) {
+            isPress = false;
+        }
+    }
 }
 
 void motion(int x, int y) {
-	if(isPress) {
-		int dx = x - prevX;
-		int dy = y - prevY;
+    if(isPress) {
+        int dx = x - prevX;
+        int dy = y - prevY;
 
-		rotX += dy / 2.0;
-		rotY += dx / 2.0;
+        rotX += dy / 2.0;
+        rotY += dx / 2.0;
 
-		prevX = x;
-		prevY = y;
+        prevX = x;
+        prevY = y;
 
-		glutPostRedisplay();
-	}
+        glutPostRedisplay();
+    }
 }
 
 void display() {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glLoadIdentity();
-	gluLookAt(0.7, 0.7, 0.7, 0, 0.1, 0, 0, 1, 0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(0.7, 0.7, 0.7, 0, 0.1, 0, 0, 1, 0);
 
-	static float light_pos[] = { 1, 1, 1, 0 };
-	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-	
-	glSetMaterial();
-	
-	glPushMatrix();
-	glRotated(rotY, 0, 1, 0);
-	glRotated(rotX, 1, 0, 0);
-	renderMesh();
-	glPopMatrix();
+    static float light_pos[] = { 1, 1, 1, 0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+    
+    glSetMaterial();
+    
+    glPushMatrix();
+    glRotated(rotY, 0, 1, 0);
+    glRotated(rotX, 1, 0, 0);
+    renderMesh();
+    glPopMatrix();
 
-	glutSwapBuffers();
+    glutSwapBuffers();
 }
 
 void reshape(int width, int height) {
-	glViewport(0, 0, width, height);
+    glViewport(0, 0, width, height);
 
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(20.0, (double)width / height, 1, 100);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(20.0, (double)width / height, 1, 100);
 
-	glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 void glInitFunc() {
-	glClearColor(0, 0, 0, 1);
+    glClearColor(0, 0, 0, 1);
 
-	// ŒõŒ¹‚Ì‰Šú‰»
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
+    // ŒõŒ¹‚Ì‰Šú‰»
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 
-	// ‚»‚Ì‚Ù‚©
-	glEnable(GL_NORMALIZE);
-	glEnable(GL_DEPTH_TEST);
+    // ‚»‚Ì‚Ù‚©
+    glEnable(GL_NORMALIZE);
+    glEnable(GL_DEPTH_TEST);
 }
 
 void glSetCallbacks() {
-	glutDisplayFunc(display);
-	glutReshapeFunc(reshape);
-	glutMouseFunc(mouse);
-	glutMotionFunc(motion);
-	glutKeyboardFunc(keyboard);
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);
+    glutKeyboardFunc(keyboard);
 }
 
 int main(int argc, char** argv) {
@@ -192,17 +192,17 @@ int main(int argc, char** argv) {
     }
     const std::string filename = argv[1];
 
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowPosition(100, 100);
-	glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
-	glutCreateWindow("L0 Mesh Denoising");
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(WIN_WIDTH, WIN_HEIGHT);
+    glutCreateWindow("L0 Mesh Denoising");
 
-	loadMesh(filename);
-	description();
+    loadMesh(filename);
+    description();
 
-	glSetCallbacks();
-	glInitFunc();
+    glSetCallbacks();
+    glInitFunc();
 
-	glutMainLoop();
+    glutMainLoop();
 }
